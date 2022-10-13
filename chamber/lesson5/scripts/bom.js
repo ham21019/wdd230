eb15beb3e199ea2869647f351616b29bebd4f043
ham21019 <ham21019@byui.edu>
2022-10-12 20:11:34 -0700

    js tweaks

--- chamber/lesson5/scripts/bom.js
+++ chamber/lesson5/scripts/bom.js
@@ -1,24 +1,30 @@
+// define default varialbes
 const list = document.querySelector('ul');
 const input = document.querySelector('input');
 const button = document.querySelector('button');
 
+// button function
 button.addEventListener('click', () => {
   const myItem = input.value;
   input.value = '';
 
+  // create HTML elements
   const listItem = document.createElement('li');
   const listText = document.createElement('span');
   const listBtn = document.createElement('button');
 
+  // add myItem to the list
   listItem.appendChild(listText);
   listText.textContent = myItem;
   listItem.appendChild(listBtn);
   listBtn.textContent = '❌';
   list.appendChild(listItem);
 
+  // delete button functionality
   listBtn.addEventListener('click', () => {
     list.removeChild(listItem);
   });
 
+  // retun focus
   input.focus();
 });
