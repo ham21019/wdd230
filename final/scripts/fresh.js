3b1ca29bba820190d8a62cb49e64746b92b28f2f
ham21019 <ham21019@byui.edu>
2022-11-26 14:27:31 -0700

    fruits testing

--- final/scripts/fresh.js
+++ final/scripts/fresh.js
@@ -1,24 +1,24 @@
 // url variable for fruityvice api
 const url = "https://www.fruityvice.com/api/fruit/all";
 
-// // apiFetch function to get JSON information
-// async function apiFetch() {
-//   try {
-//     const response = await fetch(url);
-//     if (response.ok) {
-//       const data = await response.json();
-//       console.log(data); // test output in the console
-//       displayResults(data);
-//     } else {
-//         throw Error(await response.text());
-//     }
-//   } catch (error) {
-//       console.log(error);
-//   }
-// }
+// apiFetch function to get JSON information
+async function apiFetch() {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data); // test output in the console
+      displayResults(data);
+    } else {
+        throw Error(await response.text());
+    }
+  } catch (error) {
+      console.log(error);
+  }
+}
 
-// // run function to get API info
-// apiFetch();
+// run function to get API info
+apiFetch();
 
 // function httpGet(theUrl) {
 //     let xmlHttpReq = new XMLHttpRequest();
@@ -28,12 +28,12 @@ const url = "https://www.fruityvice.com/api/fruit/all";
 
 //   console.log(httpGet(url));
 
-fetch(url)
-    .then(function (response) {
-    return response.json();
-    })
-    .then(function (jsonObject) {
-    console.table(jsonObject);
-    const fruits = jsonObject["fruits"];
-    console.log(fruits)
-    });
+// fetch(url)
+//     .then(function (response) {
+//     return response.json();
+//     })
+//     .then(function (jsonObject) {
+//     console.table(jsonObject);
+//     const fruits = jsonObject["fruits"];
+//     console.log(fruits)
+//     });
