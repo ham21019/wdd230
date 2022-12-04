77faa238b8e76c2cf8246ced255d142f861eb267
ham21019 <ham21019@byui.edu>
2022-12-03 21:27:41 -0700

    html and javascript foundation done for final

--- final/scripts/fresh.js
+++ final/scripts/fresh.js
@@ -1,40 +1,102 @@
-const requestURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
-const test = document.querySelector("#test");
+// for loop found on: // javascript help found on: https://www.codebyamir.com/blog/populate-a-select-dropdown-list-with-json
 
+// define url for fetch request
+const requestURL = "https://brotherblazzard.github.io/canvas-content/fruit.json";
+
+// define dropdown variables
+let dropdownOne = document.querySelector("#dropdown_one");
+dropdownOne.length = 0;
+let dropdownTwo = document.querySelector("#dropdown_two");
+dropdownOne.length = 0;
+let dropdownThree = document.querySelector("#dropdown_three");
+dropdownOne.length = 0;
+
+// define default option text
+let defaultOptionOne = document.createElement("option");
+defaultOptionOne.text = "Choose Your Fruit"
+let defaultOptionTwo = document.createElement("option");
+defaultOptionTwo.text = "Choose Your Fruit"
+let defaultOptionThree = document.createElement("option");
+defaultOptionThree.text = "Choose Your Fruit"
+
+
+// add default option text to top of drop downs
+dropdownOne.add(defaultOptionOne);
+dropdownOne.selectedIndex = 0;
+dropdownTwo.add(defaultOptionTwo);
+dropdownTwo.selectedIndex = 0;
+dropdownThree.add(defaultOptionThree);
+dropdownThree.selectedIndex = 0;
+
+// fetch and process json file
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);
-    const fruits = jsonObject["fruits"];
+    let optionOne;
+    let optinoTwo;
+    let optionThree;
 
-    // fruits.forEach(displayFruits);
+    for (let i = 0; i < jsonObject.length; i++) {
+      optionOne = document.createElement("option");
+      optionOne.text = jsonObject[i].name;
+      dropdownOne.add(optionOne);
+
+      optinoTwo = document.createElement("option");
+      optinoTwo.text = jsonObject[i].name;
+      dropdownTwo.add(optinoTwo);
+
+      optionThree = document.createElement("option");
+      optionThree.text = jsonObject[i].name;
+      dropdownThree.add(optionThree);
+    }
   });
 
-// // displayfruits function
-// function displayfruits(fruit) {
-//     let card = document.createElement("section");
-//     let h2 = document.createElement("h2");
-//     let portrait = document.createElement("img");
-//     let birth = document.createElement("p");
+// form submission handling
+myForm = document.querySelector("form[name=myForm]");
+myForm.querySelector("button").addEventListener('click', function() {
+  myForm.requestSubmit();
+})
+
+myForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+  console.log("Submitting form");
+  displayForm()
+})
+
+function displayForm() {
+  // gather input information
+  let formName = document.querySelector("#form_first").value;
+  let formEmail = document.querySelector("#form_email").value;
+  let formPhone = document.querySelector("#form_phone").value;
+  let formFruitOne = document.querySelector("#dropdown_one").value;
+  let formFruitTwo = document.querySelector("#dropdown_two").value;
+  let formFruitThree = document.querySelector("#dropdown_three").value;
+  let formSpecial = document.querySelector("#form_special").value;
+
+  let result = `
+    First Name: ${formName},
+    Email: ${formEmail},
+    Phone: ${formPhone},
+    Fruit #1: ${formFruitOne},
+    Fruit #2: ${formFruitTwo},
+    Fruit #3: ${formFruitThree},
+    Special Instructions: ${formSpecial}`;
   
-//     // Change the textContent property of the h2 element to contain the fruit's full name
-//     h2.textContent = `${fruit.name} ${fruit.lastname}`;
-  
-//     // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
-//     portrait.setAttribute("src", fruit.imageurl);
-//     portrait.setAttribute("alt", `Portait of ${fruit.name} ${fruit.lastname}`);
-//     portrait.setAttribute("loading", "lazy");
-    
-//     // birth place and birth date
-//     birth.textContent = `Born in ${fruit.birthplace} on ${fruit.birthdate}`;
-  
-//     // Add/append the section(card) with the h2 element
-//     card.appendChild(h2);
-//     card.appendChild(portrait);
-//     card.appendChild(birth);
-  
-//     // Add/append the existing HTML div with the cards class with the section(card)
-//     document.querySelector('div.cards').appendChild(card);
-//   }
+  document.querySelector("#new_drink").textContent = result;
+
+  // reset form
+  document.querySelector("#form_first").value = "";
+  document.querySelector("#form_email").value = "";
+  document.querySelector("#form_phone").value = "";
+  document.querySelector("#dropdown_one").value = "";
+  document.querySelector("#dropdown_two").value = "";
+  document.querySelector("#dropdown_three").value = "";
+  document.querySelector("#form_special").value = "";
+
+  // set localStorage Value or increase by one
+  let smoothieCounter = Number(window.localStorage.getItem("smoothies"));
+  smoothieCounter++;
+  localStorage.setItem("smoothies", smoothieCounter);
+}
